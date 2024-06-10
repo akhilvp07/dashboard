f4077349026ae744bc62a3fa8628ea0600bf76bd
Akhil Valsamrithan <avalsamrithan@rbbn.com>
2024-06-10 14:35:24 +0530

    pingstatus without path

--- dash.py
+++ dash.py
@@ -15,7 +15,7 @@ class DashboardHandler(http.server.BaseHTTPRequestHandler):
         if self.path == '/refresh':
             # Execute the bash command when the 'refresh' button is clicked
             print("Refresh button clicked, running sync command.")
-            subprocess.call(['~/bin/pingstatus', '--sync'])
+            subprocess.call(['pingstatus', '--sync'])
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
